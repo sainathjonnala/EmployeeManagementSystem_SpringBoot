314b72a266902862b84114dabd61f51bd34b65c0
sainathjonnala <sainath.jonnala@gmail.com>
2019-09-26 18:42:02 +0530

    Support for angular added.

--- EMS_Spring/src/main/java/com/comakeit/spring/rest/LoginRest.java
+++ EMS_Spring/src/main/java/com/comakeit/spring/rest/LoginRest.java
@@ -1,6 +1,7 @@
 package com.comakeit.spring.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
